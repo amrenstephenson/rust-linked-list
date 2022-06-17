3fd0653a3b651b8758ccfbbf4c26d9445bdbf116
Amren Stephenson <amren.h.stephenson@durham.ac.uk>
2022-06-17 21:41:25 +0100

    It actually compiles!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+struct LL<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> LL<T> {
+    fn new() -> LL<T> {
+        LL::<T> { head: None }
+    }
+    fn append(&mut self, data: T) {
+        let new_node = Some(Box::new(Node::<T> { data, next: None }));
+        match self.head {
+            None => self.head = new_node,
+            Some(ref mut i) => {
+                let mut node = i;
+                loop {
+                    match { node }.next {
+                        None => {
+                            //node.next = new_node;
+                            break;
+                        }
+                        Some(ref mut j) => {
+                            node = j;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node<T> {
+    data: T,
+    next: Option<Box<Node<T>>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut yay = LL::<i8>::new();
+    yay.append(5);
+    println!("{:?}", yay.head);
 }
